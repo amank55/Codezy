26d8c5c133df1bf210699731ec50e7ca46aebbb7
amank55 <amankumarr2005ak@gmail.com>
2025-02-13 15:54:40 +0530

    refactor: move Header, OutputPanel, and EditorPanel components to a new directory structure

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,19 @@
+import EditorPanel from "@/components/EditorPanel"
+import Header from "@/components/Header"
+import OutputPanel from "@/components/OutputPanel"
 import { SignOutButton, SignUpButton } from "@clerk/nextjs"
+
  export default function Home(){
+  
   return(
-    <div>
-    hey
+    <div className="min-h-screen">
+      <div className="max-w-[1800px] mx-auto p-4">
+          <Header/>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <OutputPanel/>
+          <EditorPanel/>
+      </div>
+      </div>
     </div>
   )
  }
