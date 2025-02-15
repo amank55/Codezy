64a22c6b273f0d697ef7825f0fbf11fe800f7dd7
amank55 <amankumarr2005ak@gmail.com>
2025-02-15 10:50:09 +0530

    feat: implement useMounted hook and integrate it into LanguageSelector and ThemeSelector components

--- my-app/src/components/provider/LanguageSelector.tsx
+++ my-app/src/components/provider/LanguageSelector.tsx
@@ -5,11 +5,11 @@ import { LANGUAGE_CONFIG } from "../constants";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import { ChevronDownIcon, Lock, Sparkles } from "lucide-react";
-//import useMounted from "@/hooks/useMounted";
+import useMounted from "../hooks/useMounted";
 
 function LanguageSelector({ hasAccess }: { hasAccess: boolean }) {
   const [isOpen, setIsOpen] = useState(false);
-  //const mounted = useMounted();
+const mounted = useMounted();
 
   const { language, setLanguage } = useCodeEditorStore();
   const dropdownRef = useRef<HTMLDivElement>(null);
@@ -33,7 +33,7 @@ function LanguageSelector({ hasAccess }: { hasAccess: boolean }) {
     setIsOpen(false);
   };
 
-  //if (!mounted) return null;
+  if (!mounted) return null;
 
   return (
     <div className="relative" ref={dropdownRef}>
