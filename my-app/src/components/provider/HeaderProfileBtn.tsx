ffa1157d6387a281af093a912fcaa2cc75a9bfa6
amank55 <amankumarr2005ak@gmail.com>
2025-02-15 09:46:14 +0530

    feat: add Monaco Editor integration and enhance HeaderProfileBtn with user authentication

--- my-app/src/components/provider/HeaderProfileBtn.tsx
+++ my-app/src/components/provider/HeaderProfileBtn.tsx
@@ -1,9 +1,25 @@
+"use client";
 
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { User } from "lucide-react";
 
 function HeaderProfileBtn() {
   return (
-    <div>HeaderProfileBtn</div>
-  )
-}
+    <>
+      <UserButton>
+        <UserButton.MenuItems>
+          <UserButton.Link
+            label="Profile"
+            labelIcon={<User className="size-4" />}
+            href="/profile"
+          />
+        </UserButton.MenuItems>
+      </UserButton>
 
-export default HeaderProfileBtn
+      <SignedOut>
+        <SignedIn/>
+      </SignedOut>
+    </>
+  );
+}
+export default HeaderProfileBtn;
