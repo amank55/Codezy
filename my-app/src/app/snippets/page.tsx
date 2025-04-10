6eba75d7d49198b53bc43656e3a1a696b6271546
amank55 <amankumarr2005ak@gmail.com>
2025-04-10 15:52:55 +0530

    feat: implement deleteSnippet, starSnippet, addComment, and deleteComment mutations; add queries for snippet and comment retrieval

---
+++ my-app/src/app/snippets/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+function page (){
+        return <div>My name is aman kumar</div>
+    
+}
+export default page ;
